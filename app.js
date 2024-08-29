8899e35c5ef37c77387bf219130ad596acb10772
aarontark <tarkington.aaron@gmail.com>
2024-08-29 01:36:38 -0500

    implented working addition function

--- app.js
+++ app.js
@@ -7,50 +7,41 @@ let equals = document.querySelector('.equals');
 let clearBtn = document.querySelector('.clear');
 
 let operands = [''];
+let operator;
 
 numbers.forEach((currentNum) => {
     currentNum.addEventListener('click', () => {
-        operands[operands.length - 1] = Number(operands[operands.length - 1] + currentNum.innerHTML);
-        console.log(operands);
-        display.innerHTML = operands[operands.length - 1];
+        if(operands.length == 1) {
+            operands[0] = Number(operands[0] + currentNum.innerHTML);
+            console.log(operands);
+            display.innerHTML = operands[0];
+        } else if (operands.length == 2) {
+            operands[operands.length - 1] = Number(operands[operands.length - 1] + currentNum.innerHTML);
+            console.log(operands);
+            display.innerHTML = operands[operands.length - 1];
+        }
     })
 })
 
 addition.addEventListener('click', () => {
-    if (operands.length == 1) {
+    operator = '+';
+    if(operands.length == 1) {
         operands.push('');
-    } else if (operands.length > 1) {
-        let operand1 = Number(operands[operands.length - 2]);
-        let operand2 = Number(operands[operands.length - 1]);
+    } else {
+        let operand1 = Number(operands[0])
+        let operand2 = Number(operands[1])
         let sum = operand1 + operand2;
+        operands.shift();
+        operands.shift();
         operands.push(sum);
-        operands.push('')
-        operands.shift();
-        operands.shift();
-        console.log(operands);
-    }
-})
-
-subtraction.addEventListener('click', () => {
-    if (operands.length == 1) {
         operands.push('');
-    } else if (operands.length > 1) {
-        let operand1 = Number(operands[operands.length - 2]);
-        let operand2 = Number(operands[operands.length - 1]);
-        let diff = operand1 - operand2;
-        operands.push(diff);
-        operands.push('')
-        operands.shift();
-        operands.shift();
-        console.log(operands);
     }
 })
 
 equals.addEventListener('click', () => {
-    display.innerHTML = operands[0];
-})
-
-clearBtn.addEventListener('click', () => {
-    display.innerHTML = 0;
-    operands = ['']
+    if(operator === '+') {
+        let sum = Number(operands[0]) + Number(operands[1]);
+        display.innerHTML = sum;
+        operands = [sum];
+    }
 })
