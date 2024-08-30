7a154d92fcf0ac0b68978c70931326d06cf54ebd
aarontark <tarkington.aaron@gmail.com>
2024-08-30 00:15:57 -0500

    implemented subtraction

--- app.js
+++ app.js
@@ -13,21 +13,25 @@ numbers.forEach((currentNum) => {
     currentNum.addEventListener('click', () => {
         if(operands.length == 1) {
             operands[0] = Number(operands[0] + currentNum.innerHTML);
-            console.log(operands);
             display.innerHTML = operands[0];
         } else if (operands.length == 2) {
             operands[operands.length - 1] = Number(operands[operands.length - 1] + currentNum.innerHTML);
-            console.log(operands);
             display.innerHTML = operands[operands.length - 1];
         }
     })
 })
 
 addition.addEventListener('click', () => {
+    if(operator != '+' && operands.length > 1) {
+        operatorSwitch();
+    }
     operator = '+';
     if(operands.length == 1) {
+        console.log('operands plus length 1')
         operands.push('');
+        console.log(operands)
     } else {
+        console.log('operands plus length 2')
         let operand1 = Number(operands[0])
         let operand2 = Number(operands[1])
         let sum = operand1 + operand2;
@@ -35,6 +39,29 @@ addition.addEventListener('click', () => {
         operands.shift();
         operands.push(sum);
         operands.push('');
+        console.log(operands);
+    }
+})
+
+subtraction.addEventListener('click', () => {
+    if(operator != '-' && operands.length > 1) {
+        operatorSwitch();
+    }
+    operator = '-';
+    if(operands.length == 1) {
+        console.log('operands minus length 1');
+        operands.push('');
+        console.log(operands);
+    } else {
+        console.log('operands minus length 2');
+        let operand1 = Number(operands[0])
+        let operand2 = Number(operands[1])
+        let difference = operand1 - operand2;
+        operands.shift();
+        operands.shift();
+        operands.push(difference);
+        operands.push('');
+        console.log(operands);
     }
 })
 
@@ -42,6 +69,25 @@ equals.addEventListener('click', () => {
     if(operator === '+') {
         let sum = Number(operands[0]) + Number(operands[1]);
         display.innerHTML = sum;
-        operands = [sum];
+        operands = [sum, ''];
+    } else if (operator == '-') {
+        let difference = Number(operands[0]) - Number(operands[1]);
+        display.innerHTML = difference;
+        operands = [difference, ''];
     }
 })
+
+function operatorSwitch() {
+    switch (operator) {
+        case "+":
+            console.log('switch');
+            console.log(operands);
+            operands = [operands[0] + operands[1]];
+            break;
+        case '-':
+            console.log('switch');
+            console.log(operands);
+            operands = [operands[0] - operands[1]];
+            break;
+    }
+}
