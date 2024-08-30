c86ab0462d5e5a8c4b3290dddfb12fb3b37c93be
aarontark <tarkington.aaron@gmail.com>
2024-08-30 05:06:33 -0500

    implemented multiplication and division

--- app.js
+++ app.js
@@ -3,20 +3,27 @@ let numbers = document.querySelectorAll('.number');
 let decimal = document.querySelector('.decimal');
 let addition = document.querySelector('.addition');
 let subtraction = document.querySelector('.subtraction');
+let multiplication = document.querySelector('.multiplication');
+let division = document.querySelector('.division');
 let equals = document.querySelector('.equals');
 let clearBtn = document.querySelector('.clear');
 
 let operands = [''];
 let operator;
 
+console.log(multiplication);
+console.log(addition);
+
 numbers.forEach((currentNum) => {
     currentNum.addEventListener('click', () => {
         if(operands.length == 1) {
             operands[0] = Number(operands[0] + currentNum.innerHTML);
             display.innerHTML = operands[0];
+            console.log(operands);
         } else if (operands.length == 2) {
             operands[operands.length - 1] = Number(operands[operands.length - 1] + currentNum.innerHTML);
             display.innerHTML = operands[operands.length - 1];
+            console.log(operands);
         }
     })
 })
@@ -27,19 +34,15 @@ addition.addEventListener('click', () => {
     }
     operator = '+';
     if(operands.length == 1) {
-        console.log('operands plus length 1')
         operands.push('');
-        console.log(operands)
     } else {
-        console.log('operands plus length 2')
-        let operand1 = Number(operands[0])
-        let operand2 = Number(operands[1])
+        let operand1 = Number(operands[0]);
+        let operand2 = Number(operands[1]);
         let sum = operand1 + operand2;
         operands.shift();
         operands.shift();
         operands.push(sum);
         operands.push('');
-        console.log(operands);
     }
 })
 
@@ -49,19 +52,52 @@ subtraction.addEventListener('click', () => {
     }
     operator = '-';
     if(operands.length == 1) {
-        console.log('operands minus length 1');
         operands.push('');
-        console.log(operands);
     } else {
-        console.log('operands minus length 2');
-        let operand1 = Number(operands[0])
-        let operand2 = Number(operands[1])
+        let operand1 = Number(operands[0]);
+        let operand2 = Number(operands[1]);
         let difference = operand1 - operand2;
         operands.shift();
         operands.shift();
         operands.push(difference);
         operands.push('');
-        console.log(operands);
+    }
+})
+
+multiplication.addEventListener('click', () => {
+    if(operator != '*' && operands.length > 1) {
+        operatorSwitch();
+    }
+    operator = '*';
+    if(operands.length == 1) {
+        operands.push('');
+    } else {
+        let operand1 = Number(operands[0]);
+        let operand2 = Number(operands[1]);
+        let product = operand1 * operand2;
+        operands.shift();
+        operands.shift();
+        operands.push(product);
+        operands.push('');
+    }
+})
+
+
+division.addEventListener('click', () => {
+    if(operator != '/' && operands.length > 1) {
+        operatorSwitch();
+    }
+    operator = '/';
+    if(operands.length == 1) {
+        operands.push('');
+    } else {
+        let operand1 = Number(operands[0]);
+        let operand2 = Number(operands[1]);
+        let quotient = operand1 / operand2;
+        operands.shift();
+        operands.shift();
+        operands.push(quotient);
+        operands.push('');
     }
 })
 
@@ -74,20 +110,32 @@ equals.addEventListener('click', () => {
         let difference = Number(operands[0]) - Number(operands[1]);
         display.innerHTML = difference;
         operands = [difference, ''];
+    } else if (operator == '*') {
+        let product = Number(operands[0]) * Number(operands[1]);
+        display.innerHTML = product;
+        operands = [product, ''];
+    } else if (operator == '/') {
+        let quotient = Number(operands[0]) / Number(operands[1]);
+        display.innerHTML = quotient;
+        operands = [quotient, ''];
     }
+    operator = '';
+    console.log(operands);
 })
 
 function operatorSwitch() {
     switch (operator) {
         case "+":
-            console.log('switch');
-            console.log(operands);
             operands = [operands[0] + operands[1]];
             break;
         case '-':
-            console.log('switch');
-            console.log(operands);
             operands = [operands[0] - operands[1]];
+            break;
+        case '*':
+            operands = [operands[0] * operands[1]];
+            break;
+        case '/':
+            operands = [operands[0] / operands[1]];
             break;
     }
 }
